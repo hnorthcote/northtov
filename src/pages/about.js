b88b836ac102ac28ec76d9cfb78a4a1b03190239
Henry <henry.northcote@billgo.com>
2022-06-26 17:10:00 -0600

    back

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Link } from 'gatsby';
 
 const AboutPage =  () => {
     return (
@@ -6,6 +7,7 @@ const AboutPage =  () => {
             <title>Todo Sobre NorthTov</title>
             <h1>Todo Sobre NorThov</h1>
             <p>Aca va ha haber un poco de descripcion sobre como se creo la compania, las metas, la cultura, los valores, etc.</p>
+            <Link to="/">Back to Home</Link>
         </main>
 
     );
