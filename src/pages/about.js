9129b2643d3a6fcf7a989ad09fc6d30d0b7b7f04
Henry <henry.northcote@billgo.com>
2022-06-26 16:06:41 -0600

    first discovery pages

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage =  () => {
+    return (
+        <main>
+            <title>Todo Sobre Nortop</title>
+            <h1>Todo Sobre Nortop</h1>
+            <p>Aca va ha haber un poco de descripcion sobre como se creo la compania, las metas, la cultura, los valores, etc.</p>
+        </main>
+
+    );
+}
+
+export default AboutPage
