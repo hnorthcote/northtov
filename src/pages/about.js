da8ed2a2317f417fa3debbeabf165f85fbc80122
Henry <henry.northcote@billgo.com>
2022-06-26 16:55:13 -0600

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ const AboutPage =  () => {
     return (
         <main>
             <title>Todo Sobre NorthTov</title>
-            <h1>Todo Sobre NorTov</h1>
+            <h1>Todo Sobre NorThov</h1>
             <p>Aca va ha haber un poco de descripcion sobre como se creo la compania, las metas, la cultura, los valores, etc.</p>
         </main>
 
